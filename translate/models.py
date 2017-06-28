Put softmax bug back + perplexity eval

translate/models.py
@@ -305,11 +305,13 @@ def global_attention(state, hidden_states, encoder, encoder_input_length, scope=
             e = compute_energy(hidden_states, state, attn_size=encoder.attn_size, **kwargs)
 
         #e -= tf.reduce_max(e, axis=1, keep_dims=True)  # FIXME
+        #mask = tf.sequence_mask(tf.cast(encoder_input_length, tf.int32), tf.shape(hidden_states)[1],
+        #                        dtype=tf.float32)
+        #exp = tf.exp(e) * mask
+        #weights = exp / tf.reduce_sum(exp, axis=-1, keep_dims=True)
+        #weighted_average = tf.reduce_sum(tf.expand_dims(weights, 2) * hidden_states, axis=1)
 
-        mask = tf.sequence_mask(tf.cast(encoder_input_length, tf.int32), tf.shape(hidden_states)[1],
-                                dtype=tf.float32)
-        exp = tf.exp(e) * mask
-        weights = exp / tf.reduce_sum(exp, axis=-1, keep_dims=True)
+        weights = tf.nn.softmax(e)
         weighted_average = tf.reduce_sum(tf.expand_dims(weights, 2) * hidden_states, axis=1)
 
         return weighted_average, weights